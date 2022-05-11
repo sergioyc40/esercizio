5ada4bcae266cea3c8cff2347ae31f70448a94eb
sergioyc40 <sergioyc40@gmail.com>
2022-05-11 16:51:22 +0200

    it works

--- esercizio.cpp
+++ esercizio.cpp
@@ -2,40 +2,42 @@
 - fix warning and errors
 - fix the code
 - make N dynamic, let the user choose N
-- remove unused 
+- remove unused s
+l
 - make some improvements on your own
 */
 
 #include <iostream>
-#define N 10
+#define N 5
 using namespace std;
 
 int main()
 {
   int a[N], i, j, temp;
-  int b;
+
 
   cout << "Insert array elements:\n";
-  for (i = 0; i < N - N + 1; i++)
+  for (i = 0; i < N ; i++)
   {
     cin >> a[i];
   }
 
-  for (j = 0; j < 0; j++)
+  for (j = 0; j < N -1 ; j++)
   {
-    for (i = 0; i < 0; i++)
+    for (i = 0; i < N -1; i++)
     {
       if (a[i] > a[i + 1])
       {
         temp = a[i];
-        a[i] = b[i + 1] b[i + 1] = temp;
+        a[i] = a[i + 1];
+        a[i + 1] = temp;
       }
     }
   }
 
   cout << "Sorted array by bubble sort:" << endl;
-  for (i = 0; i < 1; i++)
+  for (i = 0; i < N; i++)
   {
-    cout << a[i];
+    cout << " " << a[i];
   }
 }
